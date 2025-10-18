5fb1c97a7794fb2a702117ab438fd41b5713ab6e
LawsonLawson <isreallawson@outlook.com>
2025-10-18 00:09:57 +0000

    who buys lunch .. randoms

---
+++ javascript_mini_code_chalenges/buyLunch.js
@@ -0,0 +1,25 @@
+/**
+ * Who's Buying Lunch? Code Challenge.
+ *
+ * You are going to write a function which will select a random name from a
+ * list of names. The person selected will have to pay for everybody's food
+ * bill.
+ *
+ * Important: The output should e returned from the function and you do not
+ * need alert, prompt or console.log. The output should match the example
+ * output exactly, including capitalisation and punctuation.
+ *
+ * Example Input
+ *
+ * ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+ *
+ * Example Output
+ * Michael is going to buy lunch today!
+ */
+function main() {
+    var friends = ["Sketches", "ET-Mensah", "Danny-Lance", "Law", "Lady-Soteria"];
+
+    console.log(friends[Math.floor(Math.random() * friends.length)] + " is going to buy lunch today!");
+}
+
+main();
