0e3f82d7322619afb9d2a19a14d030a5d027d139
LawsonLawson <isreallawson@outlook.com>
2025-10-23 01:17:47 +0000

    higher order functions

---
+++ javascript_mini_code_chalenges/calculator.js
@@ -0,0 +1,19 @@
+function divide(firstNumber, secondNumber) {
+    return (firstNumber / secondNumber);
+}
+
+function multiply(firstNumber, secondNumber) {
+    return (firstNumber * secondNumber);
+}
+
+function add(firstNumber, secondNumber) {
+    return (firstNumber + secondNumber);
+}
+
+function subtract(firstNumber, secondNumber) {
+    return (firstNumber - secondNumber);
+}
+
+function calculator(firstNumber, secondNumber, operator) {
+    return (operator(firstNumber, secondNumber));
+}
